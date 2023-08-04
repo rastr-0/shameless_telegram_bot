76ef4b08491923da3a81dfb6a8460175e4cdacaf
rastr-0 <romanmilko123@gmail.com>
2023-08-04 13:56:13 +0000

    Add bash script for auto-restart, in case of Internet troubles

---
+++ telegram_bot_shameless/check_and_start_bot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BOT_COMMAND="python3 main.py"
+LOG_FILE="/root/shameless_telegram_bot/telegram_bot_shameless/bash_logs.log"
+
+pgrep -p "$BOT_COMMAND" > /dev/null
+
+if [ $? -eq 0]; then
+	echo "Bot is running" >> "$LOG_FILE"
+	sleep 60
+else
+	echo "Starting the bot..." >> "$LOG_FILE"
+	nohup $BOT_COMMAND
+	echo "Bot started" >> "$LOG_FILE"
+fi
